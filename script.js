00a99469aff2fcaf4661ccc09b10540630292da1
Juliana Rosa <32584367+rosaju@users.noreply.github.com>
2019-04-11 03:49:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function show() {
+    document.getElementById('area-expanded').style.display = 'flex';
+    document.getElementById('area-collapsed').style.display = "none";
+}
+
+function hide() {
+    document.getElementById('area-collapsed').style.display = 'flex';
+    document.getElementById('area-expanded').style.display = "none";
+}
